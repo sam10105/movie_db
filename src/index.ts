1815aeb506334d235ede8aa1a97dc64b427e4808
Sam Kasyoki <samkasyoki@gmail.com>
2020-06-13 20:58:02 +0300

    add port env variable

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import createServer from './server';
+import { PORT } from './config';
 
 (async () => {
   const server = createServer();
-  const { url } = await server.listen();
+  const { url } = await server.listen({ port: PORT });
 
   console.log(`🚀 Server ready at ${url}`);
 })();
